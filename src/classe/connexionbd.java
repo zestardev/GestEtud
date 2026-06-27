7bc147a8927bda30de11e2854cc07bab7acf8c2f
oceanne2283 <oceannekoura@gmail.com>
2026-06-27 15:14:06 +0000

    Ajout de la connexion à la base de données

--- src/classe/connexionbd.java
+++ src/classe/connexionbd.java
@@ -3,11 +3,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package classe;
+import java.sql.*;
 
 /**
  *
  * @author abdel
  */
 public class connexionbd {
+    private static Connection c=null;
+    public static Connection seconnecter() throws ClassNotFoundException, SQLException{
+//        charger le pilote
+        Class.forName("com.mysql.cj.jdbc.Driver");
+//        Etablir la connection
+        c=DriverManager.getConnection("jdbc:mysql://localhost:3306/gestetud","root","");
+        return c;
+    }
     
 }
